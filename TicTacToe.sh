3eff4d7b7575e1f14637732f84627d03f8697409
vegirevathi <vegirevathi@gmail.com>
2020-06-12 19:13:16 +0530

    ADDED: Assigning Symbols to the player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,3 +2,15 @@
 echo "Welcome to Tic-Toe-Tac..Get Ready.."
 
 gameBoard=(. . . . . . . . .)
+
+function assignSymbol(){
+	if [ $(( $RANDOM%2 )) -eq 1 ]
+	then 
+		playerSymbol=X
+	else
+		playerSymbol=O
+	fi
+	echo "Player Symbol is : " $playerSymbol
+}
+assignSymbol
+
