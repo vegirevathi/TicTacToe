61f0f5d2763e4121feecce42d47ca9ceb4cf12e2
vegirevathi <vegirevathi@gmail.com>
2020-06-12 21:23:17 +0530

    ADDED: Displaying 3*3 Game Board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "Welcome to Tic-Toe-Tac..Get Ready.."
 
-gameBoard=(. . . . . . . . .)
+Arr=(. . . . . . . . .)
 
 function assignSymbol(){
 	if [ $(( $RANDOM%2 )) -eq 1 ]
@@ -27,3 +27,12 @@ function toss()
 	fi
 }
 toss
+
+displayGameBoard()
+{
+  echo "r\c 1 2 3"
+  echo "1   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+  echo "2   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+  echo "3   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+}
+displayGameBoard
